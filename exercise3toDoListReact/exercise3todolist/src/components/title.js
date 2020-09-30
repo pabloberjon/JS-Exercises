99f9e1ac11df60ae0e3b50b0a170fe9f76ba7663
pablo.berjon <pablo.berjon@crcit.es>
2020-09-30 12:34:53 +0200

    first exercise with React

---
+++ exercise3toDoListReact/exercise3todolist/src/components/title.js
@@ -0,0 +1,10 @@
+import { findAllByTitle } from "@testing-library/react";
+import React from "react";
+
+function Title() {
+    return(
+        <h1 className="title">To do list <span aria-label="emoji" role="img">🔥</span></h1>
+    )
+}
+
+export default Title;
